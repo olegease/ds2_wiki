696f5b9e478dbb0584e53807516e3d33cfa8d409
Oleg'Ease'Kharchuk <olegease@gmail.com>
2026-09-14 16:45:03 +0300

    add AGPL v3 or later license

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('Happy developing ✨')
+console.log( 'Dark Souls 2 (Scholar of the First Sin) Wiki Site' );
+
+// GNU Affero General Public License v3.0 or later
+// NO WARRANTY OF ANY KIND more details at <https://www.gnu.org/licenses/>
+// SPDX-License-Identifier: AGPL-3.0-or-later
+// app: `ds2_wiki` Dark Souls 2 (Scholar of the First Sin) Wiki Site
+// Ⓒ Copyright (C) 2026 Oleg'Ease'Kharchuk ᦒ
--- public/assets/scripts.js
+++ public/assets/scripts.js
@@ -10,3 +10,9 @@ try {
 } catch ( exception ) {
     console.log( exception )
 }
+
+// GNU Affero General Public License v3.0 or later
+// NO WARRANTY OF ANY KIND more details at <https://www.gnu.org/licenses/>
+// SPDX-License-Identifier: AGPL-3.0-or-later
+// app: `ds2_wiki` Dark Souls 2 (Scholar of the First Sin) Wiki Site
+// Ⓒ Copyright (C) 2026 Oleg'Ease'Kharchuk ᦒ
