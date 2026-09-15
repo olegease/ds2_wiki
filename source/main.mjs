14030375b72b18ec913fea75f2b337f90cc5f04c
Oleg'Ease'Kharchuk <olegease@gmail.com>
2026-09-15 03:17:51 +0300

    simple server

--- source/main.mjs
+++ source/main.mjs
@@ -10,21 +10,28 @@ const App = {
     listener( ) {
         console.log( `Listen on ${this.Http}://${this.Host}:${this.Port}` );
     },
-    Public_Dir: path.join( path.dirname( fileURLToPath( import.meta.url ) ), '../public' ),
+    Valid_Pages: ['index.html', 'assets/styles.css', 'assets/scripts.js'],
     validate( request ) {
         const q = request;
-        // TODO: validate public dir also
-        console.log( q.url );
         if ( q.method !== 'GET' ) throw new Error( 'App.validate: only get method is allowed' );
-        if ( q.url !== '/' ) throw new Error( 'App.validate: only / is allowed' );
-        return 'index.html';
+        const page = q.url === '/' ? 'index.html' : q.url.substring( 1 );
+        const isPageValid = this.Valid_Pages.includes( page );
+        if ( !isPageValid ) throw new Error( `App.validate: invalid page '${page}'` );
+        console.log( `Page: ${page}` );
+        return page;
+    },
+    Public_Dir: path.join( path.dirname( fileURLToPath( import.meta.url ) ), '../public' ),
+    Mime: {
+        '.html': 'text/html',
+        '.css': 'text/css',
+        '.js': 'text/javascript',
     },
     async response( request, response ) {
         const s = response;
         try {
             const page = this.validate( request );
             const text = await readFile( path.join( this.Public_Dir, page ) );
-            s.writeHead( 200, { 'Content-Type': 'text/html' } );
+            s.writeHead( 200, { 'Content-Type': this.Mime[path.extname( page )] } );
             s.end( text, 'utf-8' );
         } catch ( exception ) {
             console.log( exception );
@@ -34,6 +41,12 @@ const App = {
     }
 };
 
-const main = createServer( async ( q, s ) => await App.response( q, s ) );
+const main = createServer( ( q, s ) => App.response( q, s ) );
 
 main.listen( App.Port, App.Host, () => App.listener( ) );
+
+// GNU Affero General Public License v3.0 or later
+// NO WARRANTY OF ANY KIND more details at <https://www.gnu.org/licenses/>
+// SPDX-License-Identifier: AGPL-3.0-or-later
+// app: `ds2_wiki` Dark Souls 2 (Scholar of the First Sin) Wiki Site
+// Ⓒ Copyright (C) 2026 Oleg'Ease'Kharchuk ᦒ
