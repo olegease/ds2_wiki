9ddbcfa3c794d2ed2a020e9269d349b1f679b2d2
Oleg'Ease'Kharchuk <olegease@gmail.com>
2026-09-16 04:59:42 +0300

    progress special node application for database requests

---
+++ source/data.mjs
@@ -0,0 +1,23 @@
+import { createServer }         from 'node:http';
+
+// separate node process for handling database api requests using `postgres` SQL
+// always return data in JSON format to support multiple fields
+// only accepts `GET` `/data/` routes
+
+// TODO: only accepts request from main port
+// TODO: write shared `mjs` file for data that used from two or more applications
+
+const Http = 'http';
+const Host = '127.0.0.1';
+const Port = 3000;
+const Listen = `${Http}://${Host}:${Port}`;
+
+const data = createServer( async (q, s) => console.log( q.method ) );
+
+data.listen( Port, `${Host}`, () => { console.log( `Listen on ${Listen}` ) } );
+
+// GNU Affero General Public License v3.0 or later
+// NO WARRANTY OF ANY KIND more details at <https://www.gnu.org/licenses/>
+// SPDX-License-Identifier: AGPL-3.0-or-later
+// app: `ds2_wiki` Dark Souls 2 (Scholar of the First Sin) Wiki Site
+// Ⓒ Copyright (C) 2026 Oleg'Ease'Kharchuk ᦒ
